198311bf9b7f0af5baa4f39c0de44d5f8f7d012a
voga <kvogap@gmail.com>
2021-01-03 20:58:30 +0700

    voga

--- koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$koneksi = mysqli_connect("db","root","example","data") or die(mysqli_error());
-
-if ($koneksi->connect_error) {
-    die("Koneksi Gagal: " . $koneksi->connect_error);
-} 
-?>
